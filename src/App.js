e516cf2af6593cd8a60e9ad5934d0436d6435e35
jdorta08 <dortajavier.jd2@gmail.com>
2023-02-07 18:23:08 -0500

    added cards passing args by props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navbar from "./Components/Navbar";
 import "./Styles/global.scss";
 import Card from "./Components/Card";
 function App() {
-  console.log(data);
   return (
     <div className="App">
       <Navbar />
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,28 @@
 import data from "./data";
 
 const Card = () => {
-  return <div className="card--container"></div>;
+  return (
+    <div className="card--container">
+      {data.map((dataCard) => (
+        <div className="content--card--wrapper">
+          {dataCard.openSpots === 0 && (
+            <div className="card--badge sould--out">SOLD OUT</div>
+          )}
+          <div className="time--img-wrapper">
+            <img src={`../Imgaes/${dataCard.image}`}></img>
+            <h3>{dataCard.title}</h3>
+          </div>
+          <div className="stats--wrapper">
+            <h4>{dataCard.country}</h4>
+            <p> ${dataCard.price}</p>
+            <div className="rating--wrapper">
+              <p> {dataCard.stats.rating}</p>
+              <span>starts from {dataCard.stats.ratingCount} reviews</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
+export default Card;
