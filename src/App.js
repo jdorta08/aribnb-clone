dbcf40841b62a9815d2164f0ce7d77388750cc6c
jdorta08 <dortajavier.jd2@gmail.com>
2023-02-11 13:15:42 -0500

    changed props and added hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Navbar from "./Components/Navbar";
 import "./Styles/global.scss";
 import Card from "./Components/Card";
+import data from "./Components/data";
+import Hero from "./Components/Hero";
+
 function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
   return (
     <div className="App">
       <Navbar />
-      <Card />
+      <section>
+        <Hero />
+      </section>
+      <section className="card--container">{cards}</section>
     </div>
   );
 }
