639187cc7ada9f2a6665c6a497c9e1bf07c47c36
jdorta08 <dortajavier.jd2@gmail.com>
2023-02-11 13:15:54 -0500

    added hero and props changed

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,29 +1,25 @@
-import data from "./data";
-
-const Card = () => {
+const Card = (props) => {
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  }
   return (
-    <div className="card--container inner-section">
-      {data.map((dataCard) => (
-        <div className="content--card--wrapper">
-          {dataCard.openSpots === 0 && (
-            <div className="card--badge sould--out">SOLD OUT</div>
-          )}
-          <div className="time--img-wrapper">
-            <img src={dataCard.image.cover}></img>
-            <h3>{dataCard.title}</h3>
-          </div>
-          <div className="stats--wrapper">
-            <h4>{dataCard.country}</h4>
-            <p>
-              <b>${dataCard.price}</b>{" "}
-            </p>
-            <div className="rating--wrapper">
-              <p> {dataCard.stats.rating}</p>
-              <span>stars from {dataCard.stats.ratingCount} reviews</span>
-            </div>
-          </div>
+    <div className="content--card--wrapper">
+      {badgeText && <div className="card--badge sould--out">{badgeText}</div>}
+      <div className="time--img-wrapper">
+        <img src={props.item.image.cover}></img>
+        <h3>{props.item.title}</h3>
+      </div>
+      <div className="stats--wrapper">
+        <h4>{props.item.country}</h4>
+        <p>
+          <b>${props.item.price}</b>{" "}
+        </p>
+        <div className="rating--wrapper">
+          <p> {props.item.stats.rating}</p>
+          <span>stars from {props.item.stats.ratingCount} reviews</span>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
---
+++ src/Components/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind host-all without
+        leaving home
+      </p>
+    </div>
+  );
+};
+export default Hero;
